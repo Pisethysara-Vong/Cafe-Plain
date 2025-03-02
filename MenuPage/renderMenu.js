427b8426432a513707a466f2d47038edd2fbdf1d
Pisethysara-Vong <pisethysaravong@gmail.com>
2025-03-02 14:21:28 +0700

    fr fr

--- MenuPage/renderMenu.js
+++ MenuPage/renderMenu.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import {getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js"
-import {firebaseConfig} from "firebaseConfig.js";
+import {firebaseConfig} from "/firebaseConfig.js";
 
 
 // Initialize Firebase
